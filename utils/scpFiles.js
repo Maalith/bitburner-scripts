cd1b05c147e8149696cede1fbd8a0575f025e16f
Maalith <157912602+Maalith@users.noreply.github.com>
2024-06-12 19:11:01 -0600

    Create scpFiles.js

---
+++ utils/scpFiles.js
@@ -0,0 +1,9 @@
+export async function main(ns) {
+  const server = ns.args[0]
+  ns.scp('basics/HostHack.js', server, 'home')
+  ns.scp('basics/TargetHack.js', server, 'home')
+  ns.scp('basics/TargetGrow.js', server, 'home')
+  ns.scp('basics/TargetWeaken.js', server, 'home')
+  ns.scp('perm_attack.js', server, 'home')
+  ns.scp('route.js', server, 'home')
+}
